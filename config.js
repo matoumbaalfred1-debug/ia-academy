ea35047c9b8be2f0a9135c40d38dcbd26ca6ba58
matoumbaalfred1-debug <matoumbaalfred1@gmail.com>
2026-05-26 10:36:14 +0300

    fix: add config.js to repo

---
+++ config.js
@@ -0,0 +1,7 @@
+// ⚠️  CE FICHIER EST SECRET — NE JAMAIS COMMITER
+// Ajoutez "config.js" dans votre .gitignore
+//
+// Remplissez vos vraies valeurs ci-dessous :
+
+const SUPABASE_URL     = 'https://pfbijoyguskdpdseunfh.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBmYmlqb3lndXNrZHBkc2V1bmZoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgyMTczMjcsImV4cCI6MjA5Mzc5MzMyN30.UNk-OJuZBdXIgDMvtp7ZLvpxQfs9Qrzu1aAcQwHhdyQ';
